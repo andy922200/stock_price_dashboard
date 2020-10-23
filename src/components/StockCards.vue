e4d1fc56b205fcabe58c3548f0ccdb5467ba98f7
andy lien <andy922200@gmail.com>
2020-10-23 18:49:17 +0800

    install axios and change template

--- src/components/StockCards.vue
+++ src/components/StockCards.vue
@@ -1,12 +1,17 @@
 <template>
   <div class="StockCards">
-    <b-card no-body class="card--custom">
+    <b-card
+      no-body
+      class="card--custom"
+      v-for="stock in stocks"
+      :key="stock.symbol"
+    >
       <div class="priceInfo bg-white text-black">
         <div class="priceInfo__Code fifty-width">
-          <p class="priceInfo__Code--custom">Company Code</p>
+          <p class="priceInfo__Code--custom">{{ stock.symbol }}</p>
         </div>
         <div class="priceInfo__detail fifty-width">
-          <p>1013.45</p>
+          <p>{{ stock.close }}</p>
         </div>
       </div>
       <div class="card__footer">
@@ -16,7 +21,14 @@
           variant="success"
           class="icon--custom"
         ></b-icon>
-        <span>1132.56 ( 111.2% )</span>
+        <span
+          >{{ stock.change }} (
+          {{
+            stock.changePercent > 0
+              ? `+${stock.changePercent}`
+              : stock.changePercent
+          }}% )</span
+        >
       </div>
     </b-card>
   </div>
@@ -24,7 +36,21 @@
 
 <script>
 export default {
-  name: "StockCards"
+  name: "StockCards",
+  data() {
+    return {
+      stocks: [
+        { symbol: "AAPL", close: 0.0, change: 0.0, changePercent: 1.05 },
+        { symbol: "GOOL", close: 0.0, change: 0.0, changePercent: -2.01 }
+      ]
+    };
+  },
+  methods: {
+    getStocksData() {
+      // let symbols = this.stocks.map(d => d.symbol);
+      // let result = [];
+    }
+  }
 };
 </script>
 
